f15c6bdb1eaee1b3afbf1463f5517d3a8e31575b
chenhui <chenhui@zhihu.com>
2018-09-12 23:36:09 +0800

    heap tree

--- BinaryTree/BinarySearchTree.py
+++ BinaryTree/BinarySearchTree.py
@@ -5,16 +5,16 @@ class TreeNode:
         self.right = None
 
 
-def searchBST(root, value, res):
+def search(root, value, res):
     if not root:
         return False
     elif root.val == value:
         res[0] = root
         return True
     elif root.val < value:
-        return searchBST(root.right, value, res)
+        return search(root.right, value, res)
     else:
-        return searchBST(root.left, value, res)
+        return search(root.left, value, res)
 
 def insert(root, value):
     if not root:
@@ -27,6 +27,49 @@ def insert(root, value):
     else:
         root.right = insert(root.right, value)
     return root  # 插入成功
+
+"""
+在二叉查找树删去一个结点，分三种情况讨论：
+
+若*p结点为叶子结点，即PL（左子树）和PR（右子树）均为空树。由于删去叶子结点不破坏整棵树的结构，则只需修改其双亲结点的指针即可。
+若*p结点只有左子树PL或右子树PR，此时只要令PL或PR直接成为其双亲结点*f的左子树（当*p是左子树）或右子树（当*p是右子树）即可，作此修改也不破坏二叉查找树的特性。
+若*p结点的左子树和右子树均不空。在删去*p之后，为保持其它元素之间的相对位置不变，可按中序遍历保持有序进行调整，可以有两种做法：其一是令*p的左子树为*f的左/右（依*p是*f的左子树还是右子树而定）子树，*s为*p左子树的最右下的结点，而*p的右子树为*s的右子树；其二是令*p的直接前驱（in-order predecessor）或直接后继（in-order successor）替代*p，然后再从二叉查找树中删去它的直接前驱（或直接后继）。
+"""
+def delete(root, value):
+    if not root:
+        return None
+    else:
+        if value == root.val:
+            root = deleteNode(root)
+        elif value < root.val:
+            root.left = delete(root.left, value)
+        else:
+            root.right = delete(root.right, value)
+        return root
+
+def deleteNode(p):
+    if not p.left and not p.right:  # 叶子节点
+        del p  # 直接删除
+        p = None
+    elif not p.right:  # 只有左子树
+        p = p.left  # 直接删除，左子节点上移
+    elif not p.left:  # 只有右子树
+        p = p.right  # 直接删除，右子节点上移
+    else:  # 左右子树都有
+        q = p
+        s = p.left
+        while s.right:
+            q = s  # 记录s的父亲节点（重要）
+            s = s.right  # s为左子树的最右节点（即小于p的最大节点）
+        p.val = s.val  # 将s的值复制到p上
+        if q != p:  # 上面while语句被执行，s为p的左子树的最右子节点
+            q.right = s.left  # 把s的左子树给其父亲的右节点（因为s上移复制到p上，所以s得左子树，顶上s的位置作为s父亲q的右子树）
+        else:  # while没被执行，s为p的左子树
+            q.left = s.left  # 重接q的左子树（因为s为p的左子节点需要上移，且s只有左子树，左子树也跟着上移就行）
+        del s
+    return p
+            
+
 
 node8 = TreeNode(8)
 node3 = TreeNode(3)
@@ -46,10 +89,6 @@ node6.left = node4
 node6.right = node7
 node14.left = node13
 
-res = [None]
-print(searchBST(node8, 14, res))
-if res[0]:
-    print(res[0].val)
 
 def inOrder(root):
     if not root:
@@ -64,11 +103,21 @@ def inOrder(root):
         if node.left:
             stack.append(node.left)
     return res
+
+
+print("search...")
+res = [None]
+print(search(node8, 14, res))
+if res[0]:
+    print(res[0].val)
+
+print("insert...")
 insert(node8, 9)
 res = inOrder(node8)
 print(res)
 
-insert(node8, 3)
+print("delete...")
+delete(node8, 3)
 res = inOrder(node8)
 print(res)
 
--- BinaryTree/堆.py
+++ BinaryTree/堆.py
@@ -1,4 +1,8 @@
-import heapq
+"""实现堆的构建，排序，插入，以及删除根节点
+大顶堆
+"""
+
+
 class heap:
     def __init__(self, nums):
         self.nums = nums
@@ -8,9 +12,9 @@ class heap:
         temp = self.nums[start]
         child = start * 2 + 1
         while child <= end:
-            if child < end and self.nums[child] > self.nums[child + 1]:
+            if child < end and self.nums[child] < self.nums[child + 1]:
                 child += 1
-            if temp <= self.nums[child]:
+            if temp >= self.nums[child]:
                 break
             self.nums[start] = self.nums[child]
             start = child
@@ -24,38 +28,22 @@ class heap:
     def insert(self, node):
         self.nums.append(node)
         child = len(self.nums) - 1
-        start = (child - 1)//2  # parent
-        while start >= 0 and node < self.nums[start]:
+        start = (child - 1) // 2
+        while start >= 0 and node > self.nums[start]:
             self.nums[child] = self.nums[start]
             child = start
-            start = (child - 1)//2
+            start = (child - 1) // 2
         self.nums[child] = node
 
-    def deleteMin2(self):
-        node = self.nums[0]
-        self.nums[0] = self.nums[-1]
-        self.nums.pop()
-        start = 0
-        child = start * 2 + 1
-        while child <= len(self.nums) - 1:
-            if child < len(self.nums) - 1 and self.nums[child] > self.nums[child + 1]:
-                child += 1
-            self.nums[start] = self.nums[child]
-            start = child
-            child = start * 2 + 1
-        return node
-
     def deleteMin(self):
-        node = self.nums[0]
         self.nums[0] = self.nums[-1]
         self.nums.pop()
         self.heapAdjust(0, len(self.nums) - 1)
-        return node
 
     def heapSort(self):
         for i in range(len(self.nums) - 1, -1, -1):
             self.nums[0], self.nums[i] = self.nums[i], self.nums[0]
-            self.heapAdjust(0, i - 1)
+            self.heapAdjust(0, i - 1)  # 注意是i-1
 
 
 nums = [9,3,7,6,5,1,10,2]
@@ -67,9 +55,10 @@ h.insert(1.5)
 print(h.nums)
 h.deleteMin()
 print(h.nums)
-h.deleteMin2()
-print(h.nums)
 
 nums = [52, 32, 73, 23, 42, 62, 99, 14, 24, 39,43, 58,65, 80,120]
 h = heap(nums)
 print(h.nums)
+
+
+
